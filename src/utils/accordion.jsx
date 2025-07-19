64989fc5557380088bef55dfb88bb01995f2b230
Vanrajsinh Rathod <vanrajrathod1102@gmail.com>
2025-07-19 11:21:30 +0530

    main

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Discover the best interest rates on the market for your real estate needs! Secure competitive financing options to maximize your investment potential. Whether you're buying a new home or refinancing, take advantage of our low rates and expert guidance. Start saving today with the top rates available!",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Prevent unstable prices by exploring fixed-rate mortgage options that offer consistent monthly payments. Safeguard your investment from market fluctuations and enjoy peace of mind with predictable expenses. Secure your financial future with our stable and reliable mortgage solutions.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Get the best price on the market for your dream home! Our expert team ensures you find the most competitive deals, maximizing your investment. Don't miss out on the opportunity to secure top value in today's real estate market. Act now and make your move with confidence!",
+  },
+];
+export default data;
